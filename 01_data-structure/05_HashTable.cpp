31c3102c845043086065a51ddceea0d253ab0d95
Lance LI <heuristack@gmail.com>
2018-05-07 22:05:20 +0800

    algo: push 2D-vector into unordered container (hash-tables)

--- 01_data-structure/05_HashTable.cpp
+++ 01_data-structure/05_HashTable.cpp
@@ -1,7 +1,9 @@
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
 #include <iostream>
 #include <string>
 #include <functional>
-#include <unordered_set>
 
 using namespace std;
 
@@ -9,7 +11,11 @@ struct T
 {
     string k;
     string v;
-    bool operator == (T const & that) const { return k == that.k && v == that.v; }
+
+    bool operator == (T const & that) const
+    {
+        return k == that.k && v == that.v;
+    }
 };
 
 struct H
@@ -23,13 +29,108 @@ struct H
     }
 };
 
+template <typename T = int>
+struct Board : vector<vector<T>>
+{
+    using Base = vector<vector<T>>;
+
+    Board(int m = 3, int n = 3) : m{m},n{n}
+    {
+        resize(m,n);
+    }
+
+    Board(vector<vector<int>> v)
+    {
+        m = v.size();
+        if (!v.empty()) {
+            auto min = v[0].size();
+            for (auto const & e : v) {
+                if (e.size() < min) {
+                    min = e.size();
+                }
+            }
+            n = min;
+        }
+        resize(m,n);
+        for (auto i = 0; i < m; i++) {
+        for (auto j = 0; j < n; j++) {
+            (*this)[i][j] = v[i][j];
+        }
+        }
+    }
+
+    auto resize(int m, int n) -> void
+    {
+        this->Base::resize(m);
+        for (auto & row : *this) {
+            row.resize(n);
+        }
+    }
+
+    int m = 0;
+    int n = 0;
+
+    friend auto operator << (ostream & s, Board const & b) -> ostream &
+    {
+        for (auto i = 0; i < b.m; i++) {
+        for (auto j = 0; j < b.n; j++) {
+            s << b[i][j] << " ";
+        }
+        s << "\n";
+        }
+        return s;
+    }
+};
+
+template<typename T = int>
+struct BoardHash
+{
+    auto operator () (Board<T> const & b) const -> size_t
+    {
+        size_t v = 0;
+        for (auto const & r : b) {
+        for (auto const & e : r) {
+            v ^= hash<T>{}(e);
+        }
+        }
+        return v;
+    }
+};
+
 int main()
 {
     unordered_set<T,H> s;
-    s.emplace(T{"Hello", "Beijing"});
-    s.emplace(T{"Bye", "Beijing"});
-    s.emplace(T{"Hello", "HongKong"});
-    cout << (s.find(T{"Hello", "Shanghai"}) == s.end()) << endl;
-    cout << (s.find(T{"Bye", "Beijing"}) == s.end()) << endl;
+    s.emplace(T{"Hello","Beijing"});
+    s.emplace(T{"Bye","Beijing"});
+    s.emplace(T{"Hello","HongKong"});
+    assert(s.find(T{"Hello","Shanghai"}) == s.end());
+    assert(s.find(T{"Bye","Beijing"}) != s.end());
+
+    Board<int> board;
+    cout << board.m << board.n << endl;
+    cout << board.size();
+    for (auto row : board) cout << row.size();
+    cout << endl;
+    cout << board;
+
+    Board<int> board_3x2(3,2);
+    Board<int> board_2x3(2,3);
+    cout << board_3x2;
+    cout << board_2x3;
+
+    cout << (board_3x2 == board_2x3) << endl;
+    cout << (board == Board(board)) << endl;
+
+    Board<int> puzzle({{1,2,3},{4,5,6},{7,8,0}});
+    cout << puzzle;
+
+    unordered_set<Board<int>,BoardHash<int>> boards;
+    boards.insert(puzzle);
+    for (auto const & e : boards) {
+        cout << e;
+    }
+    assert(boards.find(puzzle) != boards.end());
+    BoardHash<int> h;
+    cout << h(puzzle) << endl;
 }
 
