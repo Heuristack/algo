75216e37e2317039722afb4880dc1dbdbb42c03d
Lance LI <heuristack@gmail.com>
2018-07-12 15:01:45 +0800

    algo: linked-list manipulation - recursion vs iteration

--- 01_data-structure/03_LinkedList.cpp
+++ 01_data-structure/03_LinkedList.cpp
@@ -11,21 +11,52 @@ template <typename T> struct ListNode
 };
 
 template <typename Key = int, typename List = shared_ptr<ListNode<Key>>>
-void linked_list_traverse(List list, typename ListNode<Key>::TraverseOrder order = ListNode<Key>::TraverseOrder::PRE)
+void linked_list_traverse_recursive(List list, typename ListNode<Key>::TraverseOrder order = ListNode<Key>::TraverseOrder::PRE)
 {
     if (list == nullptr) return;
 
     if (order == ListNode<Key>::TraverseOrder::PRE) cout << list->data << " ";
-    linked_list_traverse(list->next, order);
+    linked_list_traverse_recursive(list->next, order);
     if (order == ListNode<Key>::TraverseOrder::POS) cout << list->data << " ";
 }
 
 template <typename Key = int, typename List = shared_ptr<ListNode<Key>>>
-List linked_list_search(List list, Key key)
+void linked_list_traverse_iterative(List list)
+{
+    while (list) {
+        cout << list->data << " ";
+        list = list->next;
+    }
+}
+
+template <typename Key = int, typename List = shared_ptr<ListNode<Key>>>
+List linked_list_search_recursive(List list, Key key)
+{
+    if (list == nullptr) return nullptr;
+    if (list->data == key) {
+        return list;
+    }
+    return linked_list_search_recursive(list->next, key);
+}
+
+template <typename Key = int, typename List = shared_ptr<ListNode<Key>>>
+List linked_list_search_iterative(List list, Key key)
 {
     while (list && list->data != key) {
         list = list->next;
     }
+    return list;
+}
+
+template <typename Key = int, typename List = shared_ptr<ListNode<Key>>>
+List linked_list_remove_recursive(List list, Key key)
+{
+    if (list == nullptr) return nullptr;
+    if (list->data == key) {
+        list = list->next;
+        return linked_list_remove_recursive(list, key);
+    }
+    list->next = linked_list_remove_recursive(list->next, key);
     return list;
 }
 
@@ -39,15 +70,29 @@ int main()
                     make_shared<LNI>(LNI{4,
                         nullptr})})})});
 
-    cout << linked_list_search(L, 3) << endl;
-    cout << L->next->next->next << endl;
+    cout << linked_list_search_recursive(L, 3) << endl;
+    cout << linked_list_search_iterative(L, 3) << endl;
+    cout << L->next->next << endl;
 
     cout << "PRE: ";
-    linked_list_traverse(L, LNI::TraverseOrder::PRE);
+    linked_list_traverse_recursive(L, LNI::TraverseOrder::PRE);
     cout << endl;
 
     cout << "POS: ";
-    linked_list_traverse(L, LNI::TraverseOrder::POS);
+    linked_list_traverse_recursive(L, LNI::TraverseOrder::POS);
+    cout << endl;
+
+    cout << "FOR: ";
+    linked_list_traverse_iterative(L);
+    cout << endl;
+
+    int i = 3;
+    auto NL = linked_list_remove_recursive(L, i);
+    cout << "RM" << i << ": ";
+    linked_list_traverse_iterative(NL);
+    cout << endl;
+    cout << "RM" << i << ": ";
+    linked_list_traverse_iterative(L);
     cout << endl;
 }
 
